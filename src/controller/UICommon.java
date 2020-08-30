247bb056772beafdba7113151ae1afa6113ad06e
FlasherLinn <39999181+FlasherLinn@users.noreply.github.com>
2020-08-31 01:12:22 +0900

    Update UICommon.java

--- src/controller/UICommon.java
+++ src/controller/UICommon.java
@@ -1,126 +1,219 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package controller;
+var A_ComboBox=document.registerElement(
+	'a-combobox',{
+		prototype:Object.create(
+			HTMLDivElement.prototype,{
+				createdCallback:{
+					value:function(){
 
-import database.DBConnection;
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import oracle.jdbc.OracleTypes;
+						var a_id=this.id;
+						this.tcb=document.createElement('input');
+						this.tcb.type='checkbox';
+						this.tcb.id="_tcp_"+a_id;
+						this.tlbl=document.createElement('label');
+						this.tlbl.id="_tlbl_"+a_id;
+						this.tlbl.innerHTML="tlbl";
+						this.tlbl.htmlFor=this.tcb.id;
 
-/**
- *
- * @author Flasher Linn
- */
-public class UICommon extends Application {
+						this.hcb=document.createElement('input');
+						this.hcb.type='checkbox';
+						this.hcb.id="_tcp_"+a_id;
+						this.hlbl=document.createElement('label');
+						this.hlbl.id="_tlbl_"+a_id;
+						this.hlbl.innerHTML="hlbl";
+						this.hlbl.htmlFor=this.hcb.id;
 
-    DBConnection dBConnection = new DBConnection();
-    ObservableList observedList = FXCollections.observableArrayList();
-    public Connection con = null;
+						this.combo=document.createElement("select");
+						this.combo.id="_combo_"+a_id;
 
-    public void start(String formname,Stage stage) throws Exception {
-//         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-//        
-//        Scene scene = new Scene(root);
-//        
-//        stage.setScene(scene);
-//        stage.show();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/pos/" + formname + ".fxml"));
-        AnchorPane root = loader.load();
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add("/UI_control/UI_design.css");
-        stage.setTitle(formname);
-        stage.setScene(scene);
-        stage.show();
-        try {
-            TableView tbv;
-            ArrayList<Node> node = getallnode(root);
-            ResultSet rs = dBConnection.getItemLabel(formname, 1);
-            HashMap<String, String> datahasmap = new HashMap<String, String>();
-            while (rs.next()) {
-                datahasmap.put(rs.getString("ITEM_ID"), rs.getString("ITEM_NAME"));
-            }
-            for (int i = 0; i < node.size(); i++) {
-                if (node.get(i).getClass().getTypeName().equals("javafx.scene.control.Label")) {
-                    Label l = (Label) node.get(i);
-                    l.setText(datahasmap.get(l.getId()));
-                }
-                if (node.get(i).getClass().getTypeName().equals("javafx.scene.control.Button")) {
-                    Button B = (Button) node.get(i);
-                    B.setText(datahasmap.get(B.getId()));
-                }
-                if (node.get(i).getClass().getTypeName().equals("javafx.scene.control.TableView")) {
-                    tbv = (TableView) node.get(i);
-                    observedList=tbv.getColumns();
-                }
+						this.hdiv=document.createElement('div');
+						this.hdiv.id="_hdiv_"+this.id;
+						this.hdiv.appendChild(this.combo);
+						this.hdiv.appendChild(this.hcb);
+						this.hdiv.appendChild(this.hlbl);
 
-                if (node.get(i).getClass().getTypeName().equals("com.sun.javafx.scene.control.skin.TableColumnHeader")) {
-                    TableColumn tbc=(TableColumn) observedList.get(2);
-                    tbc.setText(datahasmap.get(tbc.getId()));
-                }
-            }
 
-        } catch (SQLException ex) {
-        }
-    }
+						this.appendChild(this.tcb);
+						this.appendChild(this.tlbl);
+						this.appendChild(this.hdiv);
+					}
+				}
 
-    public ArrayList<Node> getallnode(Parent parent) {
-        ArrayList<Node> nodes = new ArrayList<Node>();
-        addallDescendent(parent, nodes);
-        return nodes;
-    }
+			}
+		)
+	}
+);
 
-    private static void addallDescendent(Parent parent, ArrayList<Node> nodes) {
-        for (Node node : parent.getChildrenUnmodifiable()) {
-            nodes.add(node);
-            if (node instanceof Parent) {
-                addallDescendent((Parent) node, nodes);
-            }
-        }
-    }
+A_ComboBox.prototype.setdata=function(value){
+	combo=this.querySelector("#_combo_"+this.id);
+	var maxval="";
+	value.forEach(function(value,key){
+		op=document.createElement("option");
+		op.value=key;
+		op.appendChild(document.createTextNode(value));
+		combo.appendChild(op);
+		if(maxval.length<value.length){
+			maxval=value;
+		}
+	});
+	A_ComboBox.prototype.setdatabinding(combo.id);
+	var tmp=document.createElement("span");
+	tmp.className="input-elementtmp-element";
+	tmp.innerHTML=maxval.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+	tmp.style.fontFamily="Arial,Helvetica,sans-serif";
+	document.body.appendChild(tmp);
+	var theWidth=0;
+	theWidth=tmp.getBoundingClientRect().width;
+	document.body.removeChild(tmp);
+	var style=document.createElement('style');
+	style.innerHTML=".e-sel-"+combo.id+"{width:"+((theWidth+24).toFixed(2))+"px;height:20px;}"
+	console.log(style);
+	document.head.appendChild(style);
+}
 
-    public ResultSet get_comp(String form_name, int language) {
-        try {
-            dBConnection.getconnection();
-            String command = "{call GET_COMP.GET_ITEM(?,?,?)}";
-            CallableStatement csmt = con.prepareCall(command);
-            csmt.setString(1, form_name);
-            csmt.setInt(2, language);
-            csmt.registerOutParameter(3, OracleTypes.CURSOR);
-            csmt.execute();
-            ResultSet rs = (ResultSet) csmt.getObject(3);
-            return rs;
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-    }
+A_ComboBox.prototype.getcombodata=function(value){
+	combo=this.querySelector("#_combo_"+this.id);
+	console.log(combo.options[combo.selectedIndex].text);
+}
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        throw new UnsupportedOperationException("Not suppoဗrted yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+A_ComboBox.prototype.setdatabinding=function(acom_id){
+	$(function(){
+		$.widget("custom.combobox",{
+			_create:function(){
+				this.wrapper=$("<span>")
+				.addClass("custom-combobox")
+				.insertAfter(this.element);
+				this.element.hide();
+				this._createAutocomplete();
+				this._createShowAllButton();
+			},
 
+			_createAutocomplete:function(){
+				var selected=this.element.children(":selected"),
+				value=selected.val()?selected.text():"";
+
+				this.input=$("<input>")
+					.attr("id"	,"inp"+acom_id)
+					.appendTo(this.wrapper)
+					.val(value)
+					.attr("title","")
+					.addClass("e-sel-"+acom_id)
+					.autocomplete({
+						delay:0,
+						minLength:0,
+						source:$.proxy(this,"_source"),
+						autoFocus:true
+					})
+					.tooltip({
+						classes:{"ui-tooltip":"ui-state-highlight"}
+					})
+					.removeClass("ui-autocomplete-input");
+
+				this._on(
+					this.input,{
+						autocompleteselect:function(event,ui){
+							ui.item.option.selected=true;
+							this._trigger("select",event,{
+								item:ui.item.option
+							});
+						},
+
+						autocompletechange:"_removeIfInvalid"
+					}
+				);
+			},
+
+			_createShowAllButton:function(){
+				var input=this.input,
+					wasOpen=false;
+
+					$("<a>")
+						.attr("tabIndex",-1)
+						.attr("title","ShowAllItems")
+						.tooltip()
+						.appendTo(this.wrapper)
+						.button({
+							icons:{
+								primary:"ui-icon-triangle-1-s"
+							},
+							text:false
+						})
+						.removeClass("ui-corner-all")
+						.addClass("custom-combobox-toggleui-corner-right")
+						.on("mousedown",function(){
+							wasOpen=input.autocomplete("widget").is(":visible");
+						})
+						.on("click",function(){
+							input.trigger("focus");
+
+							//Closeifalreadyvisible
+							if(wasOpen){
+								return;
+							}
+
+							//Passemptystringasvaluetosearchfor,displayingallresults
+							input.autocomplete("search","");
+						});
+			},
+
+			_source:function(request,response){
+				var matcher=new RegExp($.ui.autocomplete.escapeRegex(request.term),"i");
+				response(this.element.children("option").map(function(){
+					var text=$(this).text();
+					//if(this.value&&(!request.term||matcher.test(text)))
+					return{
+						label:text,
+						value:text,
+						option:this
+					};
+				}));
+			},
+
+			_removeIfInvalid:function(event,ui){
+
+				//Selectedanitem,nothingtodo
+				if(ui.item){
+					return;
+				}
+
+				//Searchforamatch(case-insensitive)
+				var value=this.input.val(),
+				valueLowerCase=value.toLowerCase(),
+				valid=false;
+				this.element.children("option").each(function(){
+					if($(this).text().toLowerCase()===valueLowerCase){
+						this.selected=valid=true;
+						returnfalse;
+					}
+				});
+
+				//Foundamatch,nothingtodo
+				if(valid){
+					return;
+				}
+
+				//Removeinvalidvalue
+				this.input
+				.val("")
+				.attr("title",value+"didn'tmatchanyitem")
+				.tooltip("open");
+				this.element.val("");
+				this._delay(
+					function(){
+						this.input.tooltip("close").attr("title","");
+					},
+				2500);
+				this.input.autocomplete("instance").term="";
+			},
+
+			_destroy:function(){
+				this.wrapper.remove();
+				this.element.show();
+			}
+		});
+
+		$("#"+acom_id).combobox();
+		$("#toggle").on("click",function(){
+			$("#combobox").toggle();
+		});
+	});
 }
